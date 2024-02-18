f425f7fd305486c7f01ae13c976b1f0ada86aabe
chris <chrisrolling27@gmail.com>
2024-02-17 19:16:54 -0800

    works

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-const express = require('express');
+const express = require("express");
+const port = 3000;
+const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
+
 const app = express();
-const port = 3000; 
-require('dotenv').config();
-const { Client, CheckoutAPI } = require('@adyen/api-library');
+app.use(express.json());
+app.use(express.static(__dirname));
 
-const client = new Client({apiKey: process.env.ADYEN_API_KEY, environment: "TEST"});
-const checkout = new CheckoutAPI(client);
-
-const requestOptions = { idempotencyKey: "YOUR_IDEMPOTENCY_KEY" };
-
+require("dotenv").config();
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-  });
-  
+  console.log(`Listening at http://localhost:${port}`);
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); 
-  });
+const client = new Client({
+  apiKey: process.env.ADYEN_API_KEY,
+  environment: "TEST",
+});
 
+const checkout = new CheckoutAPI(client);
 
-
-  app.post('/startSession', async (req, res) => {
-
-    checkout.session({
-        amount: Math.floor(Math.random() * 100 + 1) * 100,
-        returnUrl: 'localhost:3001',
-        reference: 1,
-        countryCode: US,
-        merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT,
-    })
+app.post("/startSession", async (req, res) => {
+  await checkout.PaymentsApi.sessions({
+    amount: Math.floor(Math.random() * 100 + 1) * 100,
+    returnUrl: "localhost:3001",
+    reference: 1,
+    countryCode: "US",
+    merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT,
+  })
     .then((response) => {
-        console.log(response);
+      console.log(response);
     })
     .catch((e) => {
-        console.log(e);
+      console.log(e);
     });
-  });
-
+});
